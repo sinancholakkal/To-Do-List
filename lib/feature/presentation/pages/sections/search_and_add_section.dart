e5d89606ad81b00bfb648b5e7ada70edf6e181bd
muhammed sinan <sinanzzsinu70@gmail.com>
2025-04-08 16:28:51 +0530

    Working in make add task UI

--- lib/feature/presentation/pages/sections/search_and_add_section.dart
+++ lib/feature/presentation/pages/sections/search_and_add_section.dart
@@ -16,48 +16,96 @@ class SearchAndAddSection extends StatelessWidget {
       child: Row(
         children: [
           Flexible(
-            child: TextFormField(
-              style: TextStyle(color: kwhite),
-              decoration: InputDecoration(
-                label: Text("🚀 Search..."),
-                filled: true,
-                fillColor: Color(0xFF262626),
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8),
-                  borderSide: BorderSide(color: Color(0xFF0D0D0D)),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8),
-                  borderSide: BorderSide(color: Color(0xFF0D0D0D)),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8),
-                  borderSide: BorderSide(color: Color(0xFF0D0D0D)),
-                ),
-              ),
-            ),
+            //Search field widget
+            child: TextFormFieldWidget(labelText: "🚀 Search...",borderColor: Color(0xFF0D0D0D),),
           ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              width: 84,
-              height: 54,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                color: Color(0xFF1E6F9F),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                mainAxisSize: MainAxisSize.min,
-                spacing: 6,
-                children: [
-                  TextWidget(text: "Add",size: 14,fontWeight: FontWeight.w700,color: kwhite,),
-                  Icon(Icons.add_circle_outline,color: kwhite,size: 18,)
-                ],
+          InkWell(
+            onTap: () {
+              showDialog(context: context, builder: (context) {
+                return Dialog(
+                  backgroundColor: Colors.transparent,
+          insetPadding: EdgeInsets.zero,
+                  child: Container(
+                    width: double.infinity,
+                    height: 300,
+                    decoration: BoxDecoration(
+                      color: Color(0xFF262626),
+                      borderRadius: BorderRadius.circular(16)
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(25),
+                      child: Column(
+                        spacing: 8,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          TextWidget(text: "Add Task",color: kwhite,fontWeight: FontWeight.w700,size: 20,),
+                          TextFormFieldWidget(labelText: "Tittle",borderColor: Color(0xFF979797),height: 1,)
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },);
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Container(
+                width: 84,
+                height: 54,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  color: Color(0xFF1E6F9F),
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  spacing: 6,
+                  children: [
+                    TextWidget(text: "Add",size: 14,fontWeight: FontWeight.w700,color: kwhite,),
+                    Icon(Icons.add_circle_outline,color: kwhite,size: 18,)
+                  ],
+                ),
               ),
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class TextFormFieldWidget extends StatelessWidget {
+  const TextFormFieldWidget({
+    super.key,
+    required this.labelText,
+    this.borderColor,
+    this.height,
+  });
+  final String labelText;
+  final Color? borderColor;
+  final double? height;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      style: TextStyle(color: kwhite,height: height),
+      decoration: InputDecoration(
+        label: Text(labelText),
+        filled: true,
+        fillColor: Color(0xFF262626),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: BorderSide(color: borderColor??kwhite),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: BorderSide(color: borderColor??kwhite),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(8),
+          borderSide: BorderSide(color: borderColor??kwhite),
+        ),
       ),
     );
   }
